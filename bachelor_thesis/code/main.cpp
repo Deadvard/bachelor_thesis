cadd52fc944175ee6de2f7778eff5d2fbefe59f2
Deadvard <edvardleiborn@telia.com>
2019-04-04 11:28:18 +0200

    window

--- bachelor_thesis/code/main.cpp
+++ bachelor_thesis/code/main.cpp
@@ -1,6 +1,32 @@
+#include <glad.c>
+#include <GLFW/glfw3.h>
 
 
 int main()
 {
+	if (!glfwInit()) return -1;
 
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "Hello World", NULL, NULL);
+	
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) return -1;
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
