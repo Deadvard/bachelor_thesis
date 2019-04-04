201b1c211a3a9df705758a4d00dc52a738db412f
Deadvard <edvardleiborn@telia.com>
2019-04-04 12:06:51 +0200

    added time

--- bachelor_thesis/code/engine.cpp
+++ bachelor_thesis/code/engine.cpp
@@ -8,9 +8,22 @@ GLFWwindow* createWindow();
 void run()
 {
 	GLFWwindow* window = createWindow();
+
+	double timestep = 1.0 / 120.0;
+	double last_time = glfwGetTime();
+	double delta_time = 0.0;
 	
 	while (!glfwWindowShouldClose(window))
 	{
+		double now = glfwGetTime();
+		delta_time += now - last_time;
+		last_time = now;
+
+		while (delta_time > timestep)
+		{
+			delta_time -= timestep;
+		}
+		
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glfwSwapBuffers(window);
