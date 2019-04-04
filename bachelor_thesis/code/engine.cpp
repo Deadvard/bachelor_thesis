9da1f9e69f07a9862085dd8327101968021addee
Deadvard <edvardleiborn@telia.com>
2019-04-04 11:58:21 +0200

    new files

---
+++ bachelor_thesis/code/engine.cpp
@@ -0,0 +1,37 @@
+#include <glad.c>
+#include <GLFW/glfw3.h>
+
+GLFWwindow* createWindow()
+{
+	if (glfwInit())
+	{
+		GLFWwindow* window = 
+			glfwCreateWindow(1280, 720, "Hello World", NULL, NULL);
+		
+		if (window)
+		{
+			glfwMakeContextCurrent(window);
+
+			if (gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+			{
+				return window;
+			}
+		}
+	}
+
+	return 0;
+}
+
+void run()
+{
+	GLFWwindow* window = createWindow();
+	
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+
+		glfwPollEvents();
+	}
+}
--- bachelor_thesis/code/main.cpp
+++ bachelor_thesis/code/main.cpp
@@ -1,32 +1,7 @@
-#include <glad.c>
-#include <GLFW/glfw3.h>
-
+#include "engine.h"
 
 int main()
 {
-	if (!glfwInit()) return -1;
-
-	GLFWwindow* window = glfwCreateWindow(1280, 720, "Hello World", NULL, NULL);
-	
-	if (!window)
-	{
-		glfwTerminate();
-		return -1;
-	}
-
-	glfwMakeContextCurrent(window);
-
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) return -1;
-
-	while (!glfwWindowShouldClose(window))
-	{
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		glfwSwapBuffers(window);
-
-		glfwPollEvents();
-	}
-
-	glfwTerminate();
+	run();
 	return 0;
 }
