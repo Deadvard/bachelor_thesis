87dad6a53d6a9970d3a8b3fa314c0255a83af060
Olle <ollenassen1992@gmail.com>
2019-04-05 12:56:52 +0200

    floor

--- bachelor_thesis/code/voxel_grid.cpp
+++ bachelor_thesis/code/voxel_grid.cpp
@@ -2,11 +2,16 @@
 
 void initialize(VoxelData* data)
 {
-	data->grid.resize(500);
+	data->grid.resize(625);
 	for (auto& cell : data->grid)
 	{
 		static int x = 0;
 		static int z = 0;
-		cell.position = glm::vec3(x++, 0, z++);
+		cell.position = glm::vec3(x++, 0, z);
+		if (x % 25 == 0)
+		{
+			x = 0;
+			z++;
+		}
 	}
 }
