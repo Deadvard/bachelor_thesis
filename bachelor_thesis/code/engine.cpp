5f86f861a11a9bf051880d765997e57d3f998dee
Olle <ollenassen1992@gmail.com>
2019-04-05 12:53:33 +0200

    stupid mistake

--- bachelor_thesis/code/engine.cpp
+++ bachelor_thesis/code/engine.cpp
@@ -111,13 +111,15 @@ GLFWwindow* createWindow(int width, int height)
 
 			if (gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 			{
+				glEnable(GL_DEPTH_TEST);
+				glDepthFunc(GL_LEQUAL);
+				glEnable(GL_CULL_FACE);
+				glEnable(GL_BLEND);
+				glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
 				return window;
 			}
-			glEnable(GL_DEPTH_TEST);
-			glDepthFunc(GL_LEQUAL);
 
-			glEnable(GL_BLEND);
-			glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 		}
 	}
 
