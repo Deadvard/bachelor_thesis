7b00aebf6c1d2a20e85b59426419e58770be17b7
Deadvard <edvardleiborn@telia.com>
2019-04-16 18:09:45 +0200

    added broken marching tetrahedra

--- bachelor_thesis/code/engine.cpp
+++ bachelor_thesis/code/engine.cpp
@@ -25,6 +25,8 @@ glm::mat4 cameraView(Camera* camera);
 void voxelsToMeshes(const VoxelData* voxelData, RenderData* renderData);
 void createPoints(const VoxelData* voxelData, RenderData* renderData);
 
+void tetraVoxelsToMeshes(const VoxelData* voxelData, RenderData* renderData);
+
 void run()
 {
 	GLFWwindow* window = createWindow(1280, 720);
@@ -38,6 +40,7 @@ void run()
 	VoxelData voxelData = {};
 	initialize(&voxelData);
 
+	//tetraVoxelsToMeshes(&voxelData, &renderData);
 	voxelsToMeshes(&voxelData, &renderData);
 	createPoints(&voxelData, &renderData);
 
@@ -334,6 +337,161 @@ void voxelsToMeshes(const VoxelData* voxelData, RenderData* renderData)
 		}
 	}
 	
+	if (triangles.size() > 0)
+	{
+		glGenVertexArrays(1, &renderData->marchingCubes.vao);
+		glBindVertexArray(renderData->marchingCubes.vao);
+		glGenBuffers(1, &renderData->marchingCubes.vbo);
+		glBindBuffer(GL_ARRAY_BUFFER, renderData->marchingCubes.vbo);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(Triangle) * triangles.size(), &triangles[0], GL_STATIC_DRAW);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (void*)0);
+		glEnableVertexAttribArray(0);
+		renderData->marchingCubes.numTriangles = (int)triangles.size();
+	}
+}
+
+struct Triangle
+{
+	glm::vec3 p[3];
+};
+
+struct Gridcell
+{
+	glm::vec3 p[8];
+	float distance[8];
+};
+
+void polygoniseTri(Gridcell g, std::vector<Triangle>& tri, int v0, int v1, int v2, int v3)
+{
+	/*
+	   Determine which of the 16 cases we have given which vertices
+	   are above or below the isosurface
+	*/
+
+	unsigned int triindex =
+		(g.distance[v0] < 0.0f) << 0 |
+		(g.distance[v1] < 0.0f) << 1 |
+		(g.distance[v2] < 0.0f) << 2 |
+		(g.distance[v3] < 0.0f) << 3;
+
+	Triangle triangle;
+	/* Form the vertices of the triangles for each case */
+	switch (triindex) {
+	case 0x00:
+	case 0x0F:
+		break;
+	case 0x0E:
+	case 0x01:
+		triangle.p[0] = interpolation(g.p[v0], g.p[v1], g.distance[v0], g.distance[v1]);
+		triangle.p[1] = interpolation(g.p[v0], g.p[v2], g.distance[v0], g.distance[v2]);
+		triangle.p[2] = interpolation(g.p[v0], g.p[v3], g.distance[v0], g.distance[v3]);
+		tri.emplace_back(triangle);
+		break;
+	case 0x0D:
+	case 0x02:
+		triangle.p[0] = interpolation(g.p[v1], g.p[v0], g.distance[v1], g.distance[v0]);
+		triangle.p[1] = interpolation(g.p[v1], g.p[v3], g.distance[v1], g.distance[v3]);
+		triangle.p[2] = interpolation(g.p[v1], g.p[v2], g.distance[v1], g.distance[v2]);
+		tri.emplace_back(triangle);
+		break;
+	case 0x0C:
+	case 0x03:
+		triangle.p[0] = interpolation(g.p[v0], g.p[v3], g.distance[v0], g.distance[v3]);
+		triangle.p[1] = interpolation(g.p[v0], g.p[v2], g.distance[v0], g.distance[v2]);
+		triangle.p[2] = interpolation(g.p[v1], g.p[v3], g.distance[v1], g.distance[v3]);
+		tri.emplace_back(triangle);
+
+		triangle.p[0] = tri[0].p[2];
+		triangle.p[1] = interpolation(g.p[v1], g.p[v2], g.distance[v1], g.distance[v2]);
+		triangle.p[2] = tri[0].p[1];
+		tri.emplace_back(triangle);
+		break;
+	case 0x0B:
+	case 0x04:
+		triangle.p[0] = interpolation(g.p[v2], g.p[v0], g.distance[v2], g.distance[v0]);
+		triangle.p[1] = interpolation(g.p[v2], g.p[v1], g.distance[v2], g.distance[v1]);
+		triangle.p[2] = interpolation(g.p[v2], g.p[v3], g.distance[v2], g.distance[v3]);
+		tri.emplace_back(triangle);
+		break;
+	case 0x0A:
+	case 0x05:
+		triangle.p[0] = interpolation(g.p[v0], g.p[v1], g.distance[v0], g.distance[v1]);
+		triangle.p[1] = interpolation(g.p[v2], g.p[v3], g.distance[v2], g.distance[v3]);
+		triangle.p[2] = interpolation(g.p[v0], g.p[v3], g.distance[v0], g.distance[v3]);
+		tri.emplace_back(triangle);
+
+		triangle.p[0] = tri[0].p[0];
+		triangle.p[1] = interpolation(g.p[v1], g.p[v2], g.distance[v1], g.distance[v2]);
+		triangle.p[2] = tri[0].p[1];
+		tri.emplace_back(triangle);
+		break;
+	case 0x09:
+	case 0x06:
+		triangle.p[0] = interpolation(g.p[v0], g.p[v1], g.distance[v0], g.distance[v1]);
+		triangle.p[1] = interpolation(g.p[v1], g.p[v3], g.distance[v1], g.distance[v3]);
+		triangle.p[2] = interpolation(g.p[v2], g.p[v3], g.distance[v2], g.distance[v3]);
+		tri.emplace_back(triangle);
+
+		triangle.p[0] = tri[0].p[0];
+		triangle.p[1] = interpolation(g.p[v0], g.p[v2], g.distance[v0], g.distance[v2]);
+		triangle.p[2] = tri[0].p[2];
+		tri.emplace_back(triangle);
+		break;
+	case 0x07:
+	case 0x08:
+		triangle.p[0] = interpolation(g.p[v3], g.p[v0], g.distance[v3], g.distance[v0]);
+		triangle.p[1] = interpolation(g.p[v3], g.p[v2], g.distance[v3], g.distance[v2]);
+		triangle.p[2] = interpolation(g.p[v3], g.p[v1], g.distance[v3], g.distance[v1]);
+		tri.emplace_back(triangle);
+		break;
+	}
+}
+
+
+void tetraVoxelsToMeshes(const VoxelData* voxelData, RenderData* renderData)
+{
+	std::vector<Triangle> triangles;
+	glm::ivec3 size = voxelData->size;
+	const float* distances = voxelData->isosurface.distances;
+
+	for (int i = 0; i < size.x * size.y * size.z; ++i)
+	{
+		int x = i % size.x;
+		int y = (i / size.x) % size.y;
+		int z = i / (size.x * size.y);
+
+		if (x < size.x - 1 && y < size.y - 1 && z < size.z - 1)
+		{
+			Gridcell grid;
+			
+			grid.p[0] = glm::vec3(x, y, z);
+			grid.p[1] = glm::vec3(x + 1, y, z);
+			grid.p[2] = glm::vec3(x + 1, y, z + 1);
+			grid.p[3] = glm::vec3(x, y, z + 1);
+
+			grid.p[4] = glm::vec3(x, y + 1, z);
+			grid.p[5] = glm::vec3(x + 1, y + 1, z);
+			grid.p[6] = glm::vec3(x + 1, y + 1, z + 1);
+			grid.p[7] = glm::vec3(x, y + 1, z + 1);
+
+			grid.distance[0] = distances[0 + i];
+			grid.distance[1] = distances[1 + i];
+			grid.distance[2] = distances[1 + i + size.x * size.y];
+			grid.distance[3] = distances[0 + i + size.x * size.y];
+			grid.distance[4] = distances[0 + i + size.x];
+			grid.distance[5] = distances[1 + i + size.x];
+			grid.distance[6] = distances[1 + i + size.x + size.x * size.y];
+			grid.distance[7] = distances[0 + i + size.x + size.x * size.y];
+
+			polygoniseTri(grid, triangles, 0, 2, 3, 7);
+			polygoniseTri(grid, triangles, 0, 2, 6, 7);
+			polygoniseTri(grid, triangles, 0, 4, 6, 7);
+			polygoniseTri(grid, triangles, 0, 6, 1, 2);
+			polygoniseTri(grid, triangles, 0, 6, 1, 4);
+			polygoniseTri(grid, triangles, 5, 6, 1, 4);
+		}
+	}
+
 	if (triangles.size() > 0)
 	{
 		glGenVertexArrays(1, &renderData->marchingCubes.vao);
