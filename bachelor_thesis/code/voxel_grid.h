5b4cbc5db87a6b7f98d25c442291d7d8a4079694
Deadvard <edvardleiborn@telia.com>
2019-04-29 14:53:12 +0200

    box and sphere

--- bachelor_thesis/code/voxel_grid.h
+++ bachelor_thesis/code/voxel_grid.h
@@ -18,6 +18,15 @@ struct VoxelData
 void initialize(VoxelData* data);
 void densityFunction(VoxelData* data);
 
-void sphere(VoxelData* data, glm::vec3 position, int radius);
+struct AABB { glm::vec3 position; glm::vec3 size; };
+
+void sphere(VoxelData* data, glm::vec3 position, float radius);
+void aabb(VoxelData* data, const AABB& aabb);
+
+
+glm::vec3 closest_point(const AABB& aabb, const glm::vec3& point);
+glm::vec3 get_min(const AABB& aabb);
+glm::vec3 get_max(const AABB& aabb);
+
 
 #endif
